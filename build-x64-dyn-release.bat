5ee62282e9d3c6430e9f1f1e3f3484f75dcacf75
Corey Stinson <cstinson@smalljoes.com>
2021-01-23 22:34:51 -0600

    Adds batch files for project build.

---
+++ build-x64-dyn-release.bat
@@ -0,0 +1,7 @@
+perl Configure VC-WIN64A zlib --prefix=D:\Projects\lib\openssl-1.1.1i ^
+--release ^
+--with-zlib-include=D:\Projects\lib\zlib-1.2.11\x64\release-dyn\include ^
+--with-zlib-lib=D:\Projects\lib\zlib-1.2.11\x64\release-dyn\lib\zlib_a.lib ^
+--libdir=x64\release-dyn
+
+
---
+++ build-x64-static-debug.bat
@@ -0,0 +1,7 @@
+perl Configure VC-WIN64A zlib --prefix=D:\Projects\lib\openssl-1.1.1i ^
+--debug ^
+--with-zlib-include=D:\Projects\lib\zlib-1.2.11\include ^
+--with-zlib-lib=D:\Projects\lib\zlib-1.2.11\lib\x64\debug-static\zlib.lib ^
+--libdir=x64\static-debug ^
+/MTd
+
